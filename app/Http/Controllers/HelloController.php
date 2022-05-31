1e88c036f7faf81b9a1df053b59120fd129120d1
重友悠里 <runorunonyanmyu@gmail.com>
2022-05-31 23:40:03 +0900

    [update]hello controller routing

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
 
-    public function index() {
-        
+    public function index($id='noname', $pass='unknown') {
+
         return <<<EOF
 <html>
 <head>
@@ -21,6 +21,10 @@ h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0
 <body>
     <h1>Index</h1>
     <p>これは、Helloコントローラのindexアクションです。</p>
+    <ul>
+        <li>ID: {$id}</li>
+        <li>PASS: {$pass}</li>
+    </ul>
 </body>
 </html>
 EOF;
