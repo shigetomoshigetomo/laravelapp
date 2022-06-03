d4681da342b6f3a2a10de447b4b1bc40fbddb45e
重友悠里 <runorunonyanmyu@gmail.com>
2022-06-04 00:17:07 +0900

    [update]routing and index.php and hellocontroller

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,63 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-// global $head, $style, $body, $end;
-// $head = '<html><head>';
-// $style = <<<EOF
-// <style>
-// body {font-size: 16px; color: #999; }
-// h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
-// </style>
-// EOF;
-// $body = '</head><body>';
-// $end = '</body></html>';
-
-// function tag($tag, $txt) {
-//     return "<{$tag}>" . $txt . "</{$tag}>";
-// }
-
 class HelloController extends Controller
 {
 
-    public function index(Request $request, Response $response) {
-    //     global $head, $style, $body, $end;
-
-    //     $html = $head . tag('title', 'Hello/Index') . $style . $body
-    //         . tag('h1', 'Index') . tag('p', 'this is Index page')
-    //         . '<a href="/hello/other">go to Other page</a>'
-    //         . $end;
-    //     return $html;
-    // }
-
-    // public function other() {
-    //     global $head, $style, $body, $end;
-
-    //     $html = $head . tag('title', 'Hello/Other') . $style . $body
-    //         . tag('h1', 'Other') . tag('p', 'this is Other page')
-    //         . $end;
-    //     return $html;
-    // }
-
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size: 16px; color: #999; }
-h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-    <h1>Single Action</h1>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-
-        $response->setContent($html);
-        return $response;
+    public function index()
+    {
+        $data = ['msg' => 'これはコントローラから渡されたメッセージです。'];
+        return view('hello.index', $data);
     }
 }
