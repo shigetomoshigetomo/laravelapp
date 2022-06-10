0f7729c810289a4f245b182b38ee2e60691c4b6e
重友悠里 <runorunonyanmyu@gmail.com>
2022-06-10 16:40:30 +0900

    [update]index.blade.php use php script

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,24 +1,24 @@
 <html>
-<head>
-  <title>Hello/Index</title>
-  <style>
-    body {font-size: 16px; color: #999; }
-    h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
-  </style>
-</head>
-<body>
-  <h1>Blade/Index</h1>
-  <p>&#064;forディレクティブの例</p>
-  <ol>
-  @foreach ($data as $item)
-  @if ($loop->first)
-  <p>※データ一覧</p><ul>
-  @endif
-  <li>No, {{$loop->iteraion}}. {{$item}}</li>
-  @if ($loop->last)
-  </ul><p>--ここまで</p>
-  @endif
-  @endforeach
-  </ol>
-</body>
+  <head>
+    <title>Hello/Index</title>
+    <style>
+      body {font-size: 16px; color: #999; }
+      h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
+    </style>
+  </head>
+  <body>
+    <h1>Blade/Index</h1>
+    <p>&#064;whileディレクティブの例</p>
+    <ol>
+      @php
+        $counter = 0;
+      @endphp
+      @while ($counter < count($data))
+        <li>{{$data[$counter]}}</li>
+        @php
+          $counter++;
+        @endphp
+      @endwhile
+    </ol>
+  </body>
 </html>
