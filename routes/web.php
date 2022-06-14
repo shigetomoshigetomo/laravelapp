b4e68b4820e19d6eb14c4d600439e665c20f3461
重友悠里 <runorunonyanmyu@gmail.com>
2022-06-14 15:23:43 +0900

    [update]routing web.php

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::get('hello', 'App\Http\Controllers\HelloController@index') -> middleware(HelloMiddleware::class);
 Route::post('hello', 'App\Http\Controllers\HelloController@post');
