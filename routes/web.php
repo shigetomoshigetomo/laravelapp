29d39c6fe0954939424296bab736b006c065be39
重友悠里 <runorunonyanmyu@gmail.com>
2022-06-15 21:26:07 +0900

    [update]web.php

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::get('hello', 'App\Http\Controllers\HelloController@index')->middleware('helo');
 Route::post('hello', 'App\Http\Controllers\HelloController@post');
