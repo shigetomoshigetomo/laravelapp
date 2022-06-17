7de3cbf5f484d3227eb560e30b882c68b3eff475
重友悠里 <runorunonyanmyu@gmail.com>
2022-06-17 23:04:29 +0900

    [update]hello controller

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -16,6 +17,16 @@ class HelloController extends Controller
 
     public function post(HelloRequest $request)
     {
-        return view('hello.index', ['msg' => '正しく入力されました！']);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+        if ($validation->fails()) {
+            return redirect('/hello')
+                            ->withErrors($validator)
+                            ->withInput();
+        }
+        return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
